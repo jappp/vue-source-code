df940f8d2713daf7e4e4c835cfa11050f713b015
pengzhaorui <jappp951@163.com>
2020-10-21 14:13:07 +0800

    component vnode

--- src/vnodeToDom.js
+++ src/vnodeToDom.js
@@ -78,3 +78,157 @@ function createAppAPI(render) {
     return app
   }
 }
+
+
+/* 组件 vnode 其实是对抽象事物的描述，这是因为我们并不会在页面上真正渲染一个 <custom-component> 标签，而是渲染组件内部定义的 HTML 标签 */
+const normalVnode = {
+  type: 'button',
+  props: { 
+    'class': 'btn',
+    style: {
+      width: '100px',
+      height: '50px'
+    }
+  },
+  children: 'click me'
+}
+const CustomComponent = {
+  // 在这里定义组件对象
+}
+const componentVnode = {
+  type: CustomComponent,
+  props: { 
+    msg: 'test'
+  }
+}
+
+/* const vnode = createVNode(rootComponent, rootProps)
+createVNode 函数的大致实现  */
+function createVNode(type, props = null, children = null) {
+  if (props) {
+    // 处理 props 相关逻辑，标准化 class 和 style
+  }
+  // 对 vnode 类型信息编码
+  const shapeFlag = isString(type)
+    ? 1 /* ELEMENT */
+    : isSuspense(type)
+    ? 128 /* SUSPENSE */
+    : isTeleport(type)
+    ? 64 /* TELEPORT */
+    : isObject(type)
+    ? 4 /* STATEFUL_COMPONENT */
+    : isFunction(type)
+    ? 2 /* FUNCTIONAL_COMPONENT */
+    : 0;
+  const vnode = {
+    type,
+    props,
+    shapeFlag,
+    // 一些其他属性
+  };
+  // 标准化子节点，把不同数据类型的 children 转成数组或者文本类型
+  normalizeChildren(vnode, children);
+  return vnode;
+}
+
+/* render(vnode, rootContainer) */
+const render = (vnode, container) => {
+  if (vnode == null) {
+    // 销毁组件
+    if (container._vnode) {
+      unmount(container._vnode, null, null, true)
+    }
+  } else {
+    // 创建或者更新组件
+    patch(container._vnode || null, vnode, container)
+  }
+  // 缓存 vnode 节点，表示已经渲染
+  container._vnode = vnode
+}
+
+/* 这个函数有两个功能，一个是根据 vnode 挂载 DOM，一个是根据新旧 vnode 更新 DOM。 */
+/*
+* 第一个参数 n1 表示旧的 vnode，当 n1 为 null 的时候，表示是一次挂载的过程
+* 第二个参数 n2 表示新的 vnode 节点，后续会根据这个 vnode 类型执行不同的处理逻辑；
+* 第三个参数 container 表示 DOM 容器，也就是 vnode 渲染生成 DOM 后，会挂载到 container 下面。
+*/
+const patch = (n1, n2, container, anchor = null, parentComponent = null, parentSuspense = null, isSVG = false, optimized = false) => {
+  // 如果存在新旧节点, 且新旧节点类型不同，则销毁旧节点
+  if (n1 && !isSameVNodeType(n1, n2)) {
+    anchor = getNextHostNode(n1)
+    unmount(n1, parentComponent, parentSuspense, true)
+    n1 = null
+  }
+  const { type, shapeFlag } = n2
+  switch (type) {
+    case Text:
+      // 处理文本节点
+      break
+    case Comment:
+      // 处理注释节点
+      break
+    case Static:
+      // 处理静态节点
+      break
+    case Fragment:
+      // 处理 Fragment 元素
+      break
+    default:
+      if (shapeFlag & 1 /* ELEMENT */) {
+        // 处理普通 DOM 元素
+        processElement(n1, n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized)
+      }
+      else if (shapeFlag & 6 /* COMPONENT */) {
+        // 处理组件
+        processComponent(n1, n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized)
+      }
+      else if (shapeFlag & 64 /* TELEPORT */) {
+        // 处理 TELEPORT
+      }
+      else if (shapeFlag & 128 /* SUSPENSE */) {
+        // 处理 SUSPENSE
+      }
+  }
+}
+
+/* 用来处理组件的 processComponent 函数的实现 */
+const processComponent = (n1, n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  if (n1 == null) {
+   // 挂载组件
+   mountComponent(n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized)
+  }
+  else {
+    // 更新组件
+    updateComponent(n1, n2, parentComponent, optimized)
+  }
+}
+
+/* 挂载组件的 mountComponent 函数的实现 */
+const mountComponent = (initialVNode, container, anchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  // 创建组件实例
+  const instance = (initialVNode.component = createComponentInstance(initialVNode, parentComponent, parentSuspense))
+  // 设置组件实例
+  setupComponent(instance)
+  // 设置并运行带副作用的渲染函数
+  setupRenderEffect(instance, initialVNode, container, anchor, parentSuspense, isSVG, optimized)
+}
+
+const setupRenderEffect = (instance, initialVNode, container, anchor, parentSuspense, isSVG, optimized) => {
+  // 创建响应式的副作用渲染函数
+  // 当组件的数据发生变化时，effect 函数包裹的内部渲染函数 componentEffect 会重新执行一遍，从而达到重新渲染组件的目的
+  // 初始渲染主要做两件事情：渲染组件生成 subTree、把 subTree 挂载到 container 中
+  instance.update = effect(function componentEffect() {
+    if (!instance.isMounted) {
+      // 渲染组件生成子树 vnode
+      const subTree = (instance.subTree = renderComponentRoot(instance))
+      // 把子树 vnode 挂载到 container 中
+      patch(null, subTree, container, anchor, instance, parentSuspense, isSVG)
+      // 保留渲染生成的子树根 DOM 节点
+      initialVNode.el = subTree.el
+      instance.isMounted = true
+    }
+    else {
+      // 更新组件
+    }
+  }, prodEffectOptions)
+}
