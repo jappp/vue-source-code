e2785fa8e6a780b9d58894d520408118c99d00a7
pengzhaorui <jappp951@163.com>
2020-10-22 10:34:47 +0800

    component vnode update

--- package/domDiff.js
+++ package/domDiff.js
@@ -86,6 +86,7 @@ function isSameVNodeType (n1, n2) {
   return n1.type === n2.type && n1.key === n2.key
 }
 
+/* 和渲染过程类似，更新过程也是一个树的深度优先遍历过程 */
 const processComponent = (n1, n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized) => {
   if (n1 == null) {
     // 挂载组件
@@ -97,7 +98,7 @@ const processComponent = (n1, n2, container, anchor, parentComponent, parentSusp
 }
 const updateComponent = (n1, n2, parentComponent, optimized) => {
   const instance = (n2.component = n1.component)
-  // 根据新旧子组件 vnode 判断是否需要更新子组件
+  // 根据新旧子组件 vnode来检测和对比vnode中的 props、chidren、dirs、transiton 等属性 判断是否需要更新子组件
   if (shouldUpdateComponent(n1, n2, parentComponent, optimized)) {
     // 新的子组件 vnode 赋值给 instance.next
     instance.next = n2
@@ -111,4 +112,29 @@ const updateComponent = (n1, n2, parentComponent, optimized) => {
     n2.component = n1.component
     n2.el = n1.el
   }
+}
+
+// 更新组件
+let { next, vnode } = instance
+// next 表示新的组件 vnode
+if (next) {
+  // 更新组件 vnode 节点信息
+  updateComponentPreRender(instance, next, optimized)
+}
+else {
+  next = vnode
+}
+const updateComponentPreRender = (instance, nextVNode, optimized) => {
+  // 新组件 vnode 的 component 属性指向组件实例
+  nextVNode.component = instance
+  // 旧组件 vnode 的 props 属性
+  const prevProps = instance.vnode.props
+  // 组件实例的 vnode 属性指向新的组件 vnode
+  instance.vnode = nextVNode
+  // 清空 next 属性，为了下一次重新渲染准备
+  instance.next = null
+  // 更新 props
+  updateProps(instance, nextVNode.props, prevProps, optimized)
+  // 更新 插槽
+  updateSlots(instance, nextVNode.children)
 }
