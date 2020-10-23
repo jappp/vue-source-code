2ae10ddd501e8a14ad66768ac1f40ff99b5ed41e
pzr <jappp951@163.com>
2020-10-23 19:56:12 +0800

    patch key

---
+++ package/domDiff2.js
@@ -0,0 +1,121 @@
+// 同步头部节点
+const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  /* 头部的索引 i、旧子节点的尾部索引 e1和新子节点的尾部索引 e2 */
+  let i = 0
+  const l2 = c2.length
+  // 旧子节点的尾部索引
+  let e1 = c1.length - 1
+  // 新子节点的尾部索引
+  let e2 = l2 - 1
+  // 1. 从头部开始同步
+  // i = 0, e1 = 3, e2 = 4
+  // (a b) c d
+  // (a b) e c d
+  while (i <= e1 && i <= e2) {
+    const n1 = c1[i]
+    const n2 = c2[i]
+    if (isSameVNodeType(n1, n2)) {
+      // 相同的节点，递归执行 patch 更新节点
+      patch(n1, n2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized)
+    }
+    else {
+      break
+    }
+    i++
+  }
+}
+
+// 同步尾部节点
+const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  let i = 0
+  const l2 = c2.length
+  // 旧子节点的尾部索引
+  let e1 = c1.length - 1
+  // 新子节点的尾部索引
+  let e2 = l2 - 1
+  // 1. 从头部开始同步
+  // i = 0, e1 = 3, e2 = 4
+  // (a b) c d
+  // (a b) e c d
+  // 2. 从尾部开始同步
+  // i = 2, e1 = 3, e2 = 4
+  // (a b) (c d)
+  // (a b) e (c d)
+  while (i <= e1 && i <= e2) {
+    const n1 = c1[e1]
+    const n2 = c2[e2]
+    if (isSameVNodeType(n1, n2)) {
+      patch(n1, n2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized)
+    }
+    else {
+      break
+    }
+    e1--
+    e2--
+  }
+}
+
+// 添加新的节点
+const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  let i = 0
+  const l2 = c2.length
+  // 旧子节点的尾部索引
+  let e1 = c1.length - 1
+  // 新子节点的尾部索引
+  let e2 = l2 - 1
+  // 1. 从头部开始同步
+  // i = 0, e1 = 3, e2 = 4
+  // (a b) c d
+  // (a b) e c d
+  // ...
+  // 2. 从尾部开始同步
+  // i = 2, e1 = 3, e2 = 4
+  // (a b) (c d)
+  // (a b) e (c d)
+  // 3. 挂载剩余的新节点
+  // i = 2, e1 = 1, e2 = 2
+  if (i > e1) {
+    if (i <= e2) {
+      const nextPos = e2 + 1
+      const anchor = nextPos < l2 ? c2[nextPos].el : parentAnchor
+      while (i <= e2) {
+        // 挂载新节点
+        patch(null, c2[i], container, anchor, parentComponent, parentSuspense, isSVG)
+        i++
+      }
+    }
+  }
+}
+
+// 删除多余节点
+const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  let i = 0
+  const l2 = c2.length
+  // 旧子节点的尾部索引
+  let e1 = c1.length - 1
+  // 新子节点的尾部索引
+  let e2 = l2 - 1
+  // 1. 从头部开始同步
+  // i = 0, e1 = 4, e2 = 3
+  // (a b) c d e
+  // (a b) d e
+  // ...
+  // 2. 从尾部开始同步
+  // i = 2, e1 = 4, e2 = 3
+  // (a b) c (d e)
+  // (a b) (d e)
+  // 3. 普通序列挂载剩余的新节点
+  // i = 2, e1 = 2, e2 = 1
+  // 不满足
+  if (i > e1) {
+  }
+  // 4. 普通序列删除多余的旧节点
+  // i = 2, e1 = 2, e2 = 1
+  else if (i > e2) {
+    while (i <= e1) {
+      // 删除节点
+      unmount(c1[i], parentComponent, parentSuspense, true)
+      i++
+    }
+  }
+}
