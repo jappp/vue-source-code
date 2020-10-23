ba55dc2be6b2855c2a8524a4d2f7b1e10a6f2379
pzr <jappp951@163.com>
2020-10-23 20:00:38 +0800

    处理未知子序列

--- package/domDiff2.js
+++ package/domDiff2.js
@@ -56,6 +56,7 @@ const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, pa
 }
 
 // 添加新的节点
+// 如果索引 i 大于尾部索引 e1 且 i 小于 e2，那么从索引 i 开始到索引 e2 之间，我们直接挂载新子树这部分的节点。
 const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
   let i = 0
   const l2 = c2.length
@@ -88,6 +89,7 @@ const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, pa
 }
 
 // 删除多余节点
+// 如果索引 i 大于尾部索引 e2，那么从索引 i 开始到索引 e1 之间，我们直接删除旧子树这部分的节点。
 const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
   let i = 0
   const l2 = c2.length
@@ -116,6 +118,162 @@ const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, pa
       // 删除节点
       unmount(c1[i], parentComponent, parentSuspense, true)
       i++
+    }
+  }
+}
+
+// 处理未知子序列 建立索引图
+const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  let i = 0
+  const l2 = c2.length
+  // 旧子节点的尾部索引
+  let e1 = c1.length - 1
+  // 新子节点的尾部索引
+  let e2 = l2 - 1
+  // 1. 从头部开始同步
+  // i = 0, e1 = 7, e2 = 7
+  // (a b) c d e f g h
+  // (a b) e c d i g h
+  // 2. 从尾部开始同步
+  // i = 2, e1 = 7, e2 = 7
+  // (a b) c d e f (g h)
+  // (a b) e c d i (g h)
+  // 3. 普通序列挂载剩余的新节点， 不满足
+  // 4. 普通序列删除多余的旧节点，不满足
+  // i = 2, e1 = 4, e2 = 5
+  // 旧子序列开始索引，从 i 开始记录
+  const s1 = i
+  // 新子序列开始索引，从 i 开始记录
+  const s2 = i //
+  // 5.1 根据 key 建立新子序列的索引图
+  const keyToNewIndexMap = new Map()
+  for (i = s2; i <= e2; i++) {
+    const nextChild = c2[i]
+    keyToNewIndexMap.set(nextChild.key, i)
+  }
+}
+
+// 更新和移除旧节点
+const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  let i = 0
+  const l2 = c2.length
+  // 旧子节点的尾部索引
+  let e1 = c1.length - 1
+  // 新子节点的尾部索引
+  let e2 = l2 - 1
+  // 1. 从头部开始同步
+  // i = 0, e1 = 7, e2 = 7
+  // (a b) c d e f g h
+  // (a b) e c d i g h
+  // 2. 从尾部开始同步
+  // i = 2, e1 = 7, e2 = 7
+  // (a b) c d e f (g h)
+  // (a b) e c d i (g h)
+  // 3. 普通序列挂载剩余的新节点，不满足
+  // 4. 普通序列删除多余的旧节点，不满足
+  // i = 2, e1 = 4, e2 = 5
+  // 旧子序列开始索引，从 i 开始记录
+  const s1 = i
+  // 新子序列开始索引，从 i 开始记录
+  const s2 = i
+  // 5.1 根据 key 建立新子序列的索引图
+  // 5.2 正序遍历旧子序列，找到匹配的节点更新，删除不在新子序列中的节点，判断是否有移动节点
+  // 新子序列已更新节点的数量
+  let patched = 0
+  // 新子序列待更新节点的数量，等于新子序列的长度
+  const toBePatched = e2 - s2 + 1
+  // 是否存在要移动的节点
+  let moved = false
+  // 用于跟踪判断是否有节点移动
+  let maxNewIndexSoFar = 0
+  // 这个数组存储新子序列中的元素在旧子序列节点的索引，用于确定最长递增子序列
+  const newIndexToOldIndexMap = new Array(toBePatched)
+  // 初始化数组，每个元素的值都是 0
+  // 0 是一个特殊的值，如果遍历完了仍有元素的值为 0，则说明这个新节点没有对应的旧节点
+  for (i = 0; i < toBePatched; i++)
+    newIndexToOldIndexMap[i] = 0
+  // 正序遍历旧子序列
+  for (i = s1; i <= e1; i++) {
+    // 拿到每一个旧子序列节点
+    const prevChild = c1[i]
+    if (patched >= toBePatched) {
+      // 所有新的子序列节点都已经更新，剩余的节点删除
+      unmount(prevChild, parentComponent, parentSuspense, true)
+      continue
+    }
+    // 查找旧子序列中的节点在新子序列中的索引
+    let newIndex = keyToNewIndexMap.get(prevChild.key)
+    if (newIndex === undefined) {
+      // 找不到说明旧子序列已经不存在于新子序列中，则删除该节点
+      unmount(prevChild, parentComponent, parentSuspense, true)
+    }
+    else {
+      // 更新新子序列中的元素在旧子序列中的索引，这里加 1 偏移，是为了避免 i 为 0 的特殊情况，影响对后续最长递增子序列的求解
+      newIndexToOldIndexMap[newIndex - s2] = i + 1
+      // maxNewIndexSoFar 始终存储的是上次求值的 newIndex，如果不是一直递增，则说明有移动
+      if (newIndex >= maxNewIndexSoFar) {
+        maxNewIndexSoFar = newIndex
+      }
+      else {
+        moved = true
+      }
+      // 更新新旧子序列中匹配的节点
+      patch(prevChild, c2[newIndex], container, null, parentComponent, parentSuspense, isSVG, optimized)
+      patched++
+    }
+  }
+}
+
+// 3移动和挂载新节点
+const patchKeyedChildren = (c1, c2, container, parentAnchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  let i = 0
+  const l2 = c2.length
+  // 旧子节点的尾部索引
+  let e1 = c1.length - 1
+  // 新子节点的尾部索引
+  let e2 = l2 - 1
+  // 1. 从头部开始同步
+  // i = 0, e1 = 6, e2 = 7
+  // (a b) c d e f g
+  // (a b) e c d h f g
+  // 2. 从尾部开始同步
+  // i = 2, e1 = 6, e2 = 7
+  // (a b) c (d e)
+  // (a b) (d e)
+  // 3. 普通序列挂载剩余的新节点， 不满足
+  // 4. 普通序列删除多余的节点，不满足
+  // i = 2, e1 = 4, e2 = 5
+  // 旧子节点开始索引，从 i 开始记录
+  const s1 = i
+  // 新子节点开始索引，从 i 开始记录
+  const s2 = i //
+  // 5.1 根据 key 建立新子序列的索引图
+  // 5.2 正序遍历旧子序列，找到匹配的节点更新，删除不在新子序列中的节点，判断是否有移动节点
+  // 5.3 移动和挂载新节点
+  // 仅当节点移动时生成最长递增子序列
+  const increasingNewIndexSequence = moved
+    ? getSequence(newIndexToOldIndexMap)
+    : EMPTY_ARR
+  let j = increasingNewIndexSequence.length - 1
+  // 倒序遍历以便我们可以使用最后更新的节点作为锚点
+  for (i = toBePatched - 1; i >= 0; i--) {
+    const nextIndex = s2 + i
+    const nextChild = c2[nextIndex]
+    // 锚点指向上一个更新的节点，如果 nextIndex 超过新子节点的长度，则指向 parentAnchor
+    const anchor = nextIndex + 1 < l2 ? c2[nextIndex + 1].el : parentAnchor
+    if (newIndexToOldIndexMap[i] === 0) {
+      // 挂载新的子节点
+      patch(null, nextChild, container, anchor, parentComponent, parentSuspense, isSVG)
+    }
+    else if (moved) {
+      // 没有最长递增子序列（reverse 的场景）或者当前的节点索引不在最长递增子序列中，需要移动
+      if (j < 0 || i !== increasingNewIndexSequence[j]) {
+        move(nextChild, container, anchor, 2)
+      }
+      else {
+        // 倒序递增子序列
+        j--
+      }
     }
   }
 }
