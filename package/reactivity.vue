edda64d3b5cd41427117cb24d391854e58a0d7dd
pzr <jappp951@163.com>
2020-10-30 19:41:02 +0800

    响应式内部原理

---
+++ package/reactivity.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <button @click="random">Random msg</button>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'msg reactive'
+      }
+    },
+    methods: {
+      random() {
+        this.msg = Math.random()
+      }
+    }
+  }
+</script>
