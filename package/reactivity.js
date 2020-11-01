3b8d0cfa00bf0feb83b831902e40e9b03e5afcc3
pzr <jappp951@163.com>
2020-11-01 23:33:10 +0800

    响应式原理

--- package/reactivity.js
+++ package/reactivity.js
@@ -125,3 +125,203 @@ function track(target, type, key) {
     activeEffect.deps.push(dep)
   }
 }
+
+
+// 派发通知发生在数据更新的阶段 ，由于我们用 Proxy API 劫持了数据对象
+function createSetter() {
+  return function set(target, key, value, receiver) {
+    const oldValue = target[key]
+    value = toRaw(value)
+    const hadKey = hasOwn(target, key)
+    const result = Reflect.set(target, key, value, receiver)
+    // 如果目标的原型链也是一个 proxy，通过 Reflect.set 修改原型链上的属性会再次触发 setter，这种情况下就没必要触发两次 trigger 了
+    if (target === toRaw(receiver)) {
+      if (!hadKey) {
+        trigger(target, "add" /* ADD */, key, value)
+      }
+      else if (hasChanged(value, oldValue)) {
+        trigger(target, "set" /* SET */, key, value, oldValue)
+      }
+    }
+    return result
+  }
+}
+
+// 原始数据对象 map
+const targetMap = new WeakMap()
+function trigger(target, type, key, newValue) {
+  // 通过 targetMap 拿到 target 对应的依赖集合
+  const depsMap = targetMap.get(target)
+  if (!depsMap) {
+    // 没有依赖，直接返回
+    return
+  }
+  // 创建运行的 effects 集合
+  const effects = new Set()
+  // 添加 effects 的函数
+  const add = (effectsToAdd) => {
+    if (effectsToAdd) {
+      effectsToAdd.forEach(effect => {
+        effects.add(effect)
+      })
+    }
+  }
+  // SET | ADD | DELETE 操作之一，添加对应的 effects
+  if (key !== void 0) {
+    add(depsMap.get(key))
+  }
+  const run = (effect) => {
+    // 调度执行
+    if (effect.options.scheduler) {
+      effect.options.scheduler(effect)
+    }
+    else {
+      // 直接运行
+      effect()
+    }
+  }
+  // 遍历执行 effects
+  effects.forEach(run)
+}
+
+// 全局 effect 栈
+const effectStack = []
+// 当前激活的 effect
+let activeEffect
+function effect(fn, options = EMPTY_OBJ) {
+  if (isEffect(fn)) {
+    // 如果 fn 已经是一个 effect 函数了，则指向原始函数
+    fn = fn.raw
+  }
+  // 创建一个 wrapper，它是一个响应式的副作用的函数
+  const effect = createReactiveEffect(fn, options)
+  if (!options.lazy) {
+    // lazy 配置，计算属性会用到，非 lazy 则直接执行一次
+    effect()
+  }
+  return effect
+}
+function createReactiveEffect(fn, options) {
+  const effect = function reactiveEffect(...args) {
+    if (!effect.active) {
+      // 非激活状态，则判断如果非调度执行，则直接执行原始函数。
+      return options.scheduler ? undefined : fn(...args)
+    }
+    if (!effectStack.includes(effect)) {
+      // 清空 effect 引用的依赖
+      cleanup(effect)
+      try {
+        // 开启全局 shouldTrack，允许依赖收集
+        enableTracking()
+        // 压栈
+        effectStack.push(effect)
+        activeEffect = effect
+        // 执行原始函数
+        return fn(...args)
+      }
+      finally {
+        // 出栈
+        effectStack.pop()
+        // 恢复 shouldTrack 开启之前的状态
+        resetTracking()
+        // 指向栈最后一个 effect
+        activeEffect = effectStack[effectStack.length - 1]
+      }
+    }
+  }
+  effect.id = uid++
+  // 标识是一个 effect 函数
+  effect._isEffect = true
+  // effect 自身的状态
+  effect.active = true
+  // 包装的原始函数
+  effect.raw = fn
+  // effect 对应的依赖，双向指针，依赖包含对 effect 的引用，effect 也包含对依赖的引用
+  effect.deps = []
+  // effect 的相关配置
+  effect.options = options
+  return effect
+}
+
+
+function readonly(target) {
+  return createReactiveObject(target, true, readonlyHandlers, readonlyCollectionHandlers)
+}
+function createReactiveObject(target, isReadonly, baseHandlers, collectionHandlers) {
+if (!isObject(target)) {
+  // 目标必须是对象或数组类型
+  if ((process.env.NODE_ENV !== 'production')) {
+    console.warn(`value cannot be made reactive: ${String(target)}`)
+  }
+  return target
+}
+if (target.__v_raw && !(isReadonly && target.__v_isReactive)) {
+  // target 已经是 Proxy 对象，直接返回
+  // 有个例外，如果是 readonly 作用于一个响应式对象，则继续
+  return target
+}
+if (hasOwn(target, isReadonly ? "__v_readonly" /* readonly */ : "__v_reactive" /* reactive */)) {
+  // target 已经有对应的 Proxy 了
+  return isReadonly ? target.__v_readonly : target.__v_reactive
+}
+// 只有在白名单里的数据类型才能变成响应式
+if (!canObserve(target)) {
+  return target
+}
+// 利用 Proxy 创建响应式
+const observed = new Proxy(target, collectionTypes.has(target.constructor) ? collectionHandlers : baseHandlers)
+// 给原始数据打个标识，说明它已经变成响应式，并且有对应的 Proxy 了
+def(target, isReadonly ? "__v_readonly" /* readonly */ : "__v_reactive" /* reactive */, observed)
+return observed
+}
+
+const readonlyHandlers = {
+  get: readonlyGet,
+  has,
+  ownKeys,
+  set(target, key) {
+    if ((process.env.NODE_ENV !== 'production')) {
+      console.warn(`Set operation on key "${String(key)}" failed: target is readonly.`, target)
+    }
+    return true
+  },
+  deleteProperty(target, key) {
+    if ((process.env.NODE_ENV !== 'production')) {
+      console.warn(`Delete operation on key "${String(key)}" failed: target is readonly.`, target)
+    }
+    return true
+  }
+}
+
+function ref(value) {
+  return createRef(value)
+}
+const convert = (val) => isObject(val) ? reactive(val) : val
+function createRef(rawValue) {
+  if (isRef(rawValue)) {
+    // 如果传入的就是一个 ref，那么返回自身即可，处理嵌套 ref 的情况。
+    return rawValue
+  }
+  // 如果是对象或者数组类型，则转换一个 reactive 对象。
+  let value = convert(rawValue)
+  const r = {
+    __v_isRef: true,
+    get value() {
+      // getter
+      // 依赖收集，key 为固定的 value
+      track(r, "get" /* GET */, 'value')
+      return value
+    },
+    set value(newVal) {
+      // setter，只处理 value 属性的修改
+      if (hasChanged(toRaw(newVal), rawValue)) {
+        // 判断有变化后更新值
+        rawValue = newVal
+        value = convert(newVal)
+        // 派发通知
+        trigger(r, "set" /* SET */, 'value', void 0)
+      }
+    }
+  }
+  return r
+}
