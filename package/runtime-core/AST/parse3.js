4505c65e6dd1515db5e4d7256fc596fbd07a0f26
pengzhaorui <jappp951@163.com>
2020-11-24 18:50:41 +0800

    节点转换

--- package/runtime-core/AST/parse3.js
+++ package/runtime-core/AST/parse3.js
@@ -223,4 +223,93 @@ function createStructuralDirectiveTransform(name, fn) {
 
 }
 
+function processIf(node, dir, context, processCodegen) {
+
+  if (dir.name === 'if') {
+    // 创建分支节点
+    const branch = createIfBranch(node, dir)
+    // 创建 IF 节点，替换当前节点
+    const ifNode = {
+      type: 9 /* IF */,
+      loc: node.loc,
+      branches: [branch]
+    }
+    context.replaceNode(ifNode)
+    if (processCodegen) {
+      return processCodegen(ifNode, branch, true)
+    }
+  }
+
+  else {
+
+    // 处理 v-if 相邻节点，比如 v-else-if 和 v-else
+
+    const siblings = context.parent.children
+
+    let i = siblings.indexOf(node)
+
+    while (i-- >= -1) {
+
+      const sibling = siblings[i]
+
+      if (sibling && sibling.type === 9 /* IF */) {
+
+        // 把节点移动到 IF 节点的 branches 中
+
+        context.removeNode()
+
+        const branch = createIfBranch(node, dir)
+
+        sibling.branches.push(branch)
+
+        const onExit = processCodegen && processCodegen(sibling, branch, false)
+
+        // 因为分支已被删除，所以它的子节点需要在这里遍历
+
+        traverseNode(branch, context)
+
+        // 执行退出函数
+
+        if (onExit)
+
+          onExit()
+
+        // 恢复 currentNode 为 null，因为它已经被移除
+
+        context.currentNode = null
+
+      }
+
+      else {
+
+        context.onError(createCompilerError(28 /* X_V_ELSE_NO_ADJACENT_IF */, node.loc))
+
+      }
+
+      break
+
+    }
+
+  }
+
+}
+
+
+function createIfBranch(node, dir) {
+
+  return {
+
+    type: 10 /* IF_BRANCH */,
+
+    loc: node.loc,
+
+    condition: dir.name === 'else' ? undefined : dir.exp,
+
+    children: node.tagType === 3 /* TEMPLATE */ ? node.children : [node]
+
+  }
+
+}
+
+
 
