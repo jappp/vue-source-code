54f755ec3cbe12b65bd7fdd9ae64cb02f69cab0a
pzr <jappp951@163.com>
2020-12-26 23:27:22 +0800

    提交

--- package/src/Components/transition.js
+++ package/src/Components/transition.js
@@ -218,3 +218,120 @@ function resolveTransitionHooks(vnode, props, state, instance) {
   }
   return hooks
 }
+
+function resolveTransitionProps(rawProps) {
+  let { name = 'v', type, css = true, duration, enterFromClass = `${name}-enter-from`, enterActiveClass = `${name}-enter-active`, enterToClass = `${name}-enter-to`, appearFromClass = enterFromClass, appearActiveClass = enterActiveClass, appearToClass = enterToClass, leaveFromClass = `${name}-leave-from`, leaveActiveClass = `${name}-leave-active`, leaveToClass = `${name}-leave-to` } = rawProps
+  const baseProps = {}
+  for (const key in rawProps) {
+    if (!(key in DOMTransitionPropsValidators)) {
+      baseProps[key] = rawProps[key]
+    }
+  }
+  if (!css) {
+    return baseProps
+  }
+  const durations = normalizeDuration(duration)
+  const enterDuration = durations && durations[0]
+  const leaveDuration = durations && durations[1]
+  const { onBeforeEnter, onEnter, onEnterCancelled, onLeave, onLeaveCancelled, onBeforeAppear = onBeforeEnter, onAppear = onEnter, onAppearCancelled = onEnterCancelled } = baseProps
+  const finishEnter = (el, isAppear, done) => {
+    removeTransitionClass(el, isAppear ? appearToClass : enterToClass)
+    removeTransitionClass(el, isAppear ? appearActiveClass : enterActiveClass)
+    done && done()
+  }
+  const finishLeave = (el, done) => {
+    removeTransitionClass(el, leaveToClass)
+    removeTransitionClass(el, leaveActiveClass)
+    done && done()
+  }
+  const makeEnterHook = (isAppear) => {
+    return (el, done) => {
+      const hook = isAppear ? onAppear : onEnter
+      const resolve = () => finishEnter(el, isAppear, done)
+      hook && hook(el, resolve)
+      nextFrame(() => {
+        removeTransitionClass(el, isAppear ? appearFromClass : enterFromClass)
+        addTransitionClass(el, isAppear ? appearToClass : enterToClass)
+        if (!(hook && hook.length > 1)) {
+          if (enterDuration) {
+            setTimeout(resolve, enterDuration)
+          }
+          else {
+            whenTransitionEnds(el, type, resolve)
+          }
+        }
+      })
+    }
+  }
+  return extend(baseProps, {
+    onBeforeEnter(el) {
+      onBeforeEnter && onBeforeEnter(el)
+      addTransitionClass(el, enterActiveClass)
+      addTransitionClass(el, enterFromClass)
+    },
+    onBeforeAppear(el) {
+      onBeforeAppear && onBeforeAppear(el)
+      addTransitionClass(el, appearActiveClass)
+      addTransitionClass(el, appearFromClass)
+    },
+    onEnter: makeEnterHook(false),
+    onAppear: makeEnterHook(true),
+    onLeave(el, done) {
+      const resolve = () => finishLeave(el, done)
+      addTransitionClass(el, leaveActiveClass)
+      addTransitionClass(el, leaveFromClass)
+      nextFrame(() => {
+        removeTransitionClass(el, leaveFromClass)
+        addTransitionClass(el, leaveToClass)
+        if (!(onLeave && onLeave.length > 1)) {
+          if (leaveDuration) {
+            setTimeout(resolve, leaveDuration)
+          }
+          else {
+            whenTransitionEnds(el, type, resolve)
+          }
+        }
+      })
+      onLeave && onLeave(el, resolve)
+    },
+    onEnterCancelled(el) {
+      finishEnter(el, false)
+      onEnterCancelled && onEnterCancelled(el)
+    },
+    onAppearCancelled(el) {
+      finishEnter(el, true)
+      onAppearCancelled && onAppearCancelled(el)
+    },
+    onLeaveCancelled(el) {
+      finishLeave(el)
+      onLeaveCancelled && onLeaveCancelled(el)
+    }
+  })
+}
+
+const makeEnterHook = (isAppear) => {
+  return (el, done) => {
+    const hook = isAppear ? onAppear : onEnter
+    const resolve = () => finishEnter(el, isAppear, done)
+    hook && hook(el, resolve)
+    nextFrame(() => {
+      removeTransitionClass(el, isAppear ? appearFromClass : enterFromClass)
+      addTransitionClass(el, isAppear ? appearToClass : enterToClass)
+      if (!(hook && hook.length > 1)) {
+        if (enterDuration) {
+          setTimeout(resolve, enterDuration)
+        }
+        else {
+          whenTransitionEnds(el, type, resolve)
+        }
+      }
+    })
+  }
+}
+
+const finishEnter = (el, isAppear, done) => {
+  removeTransitionClass(el, isAppear ? appearToClass : enterToClass)
+  removeTransitionClass(el, isAppear ? appearActiveClass : enterActiveClass)
+  done && done()
+}
+
